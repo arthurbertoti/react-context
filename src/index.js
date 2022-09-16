56c2c219d1d5c995e361c23086948ca50461c256
Arthur G. Bertoti <artgbertoti@gmail.com>
2022-09-16 00:58:28 -0300

    início do curso

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { StyledEngineProvider } from '@mui/material/styles';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from 'pages/Login';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2A9F85'
+    },
+    secondary: {
+      main: '#FF7070'
+    },
+  }
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Login />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
